cb43fd6548eef85e881468fcc5854f9d642f0912
Leonardo Vinsen <leonardovinsen@gmail.com>
2023-06-04 17:12:51 +0700

    feat:  [error, middleware] introduce error handling middleware

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { ValidationError } from '../error/validationError';
+import { AlreadyExistsError } from '../error/alreadyExistsError';
+import { NotFoundError } from '../error/notFoundError';
+import { CustomError } from '../error/customError';
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    let statusCode: number;
+    if (err instanceof ValidationError) {
+        statusCode = 400;
+    } else if (err instanceof AlreadyExistsError) {
+        statusCode = 400;
+    } else if (err instanceof NotFoundError) {
+        statusCode = 404;
+    } else if (err instanceof CustomError) {
+        statusCode = err.HttpStatusCode;
+    } else {
+        statusCode = 500;
+    }
+
+    return res.status(statusCode).json({
+        "message": err.message
+    });
+};
