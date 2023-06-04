bb30cf6873519d41162ea52b017004b2615c369b
Leonardo Vinsen <leonardovinsen@gmail.com>
2023-06-05 01:02:02 +0700

    feat: [database/seed] add seed data for table books

---
+++ src/database/seed/table-books-seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO books (`title`, `synopsis`, `author`, `price`)
+        VALUES  ('Deep Work', 'One of the most valuable skills in our economy is becoming increasingly rare. If you master this skill, you will achieve extraordinary results.', 'Cal Newport', 2220),
+                ('The Making of a Manager: What to Do When Everyone Looks to You', 'Whether you are new to the job, a veteran leader, or looking to be promoted, this is the handbook you need to be the kind of manager you wish you had.', 'Julie Zhuo', 1230),
+                ('The Art of War', 'The Art of War is a book which should be used to gain advantage of opponents in the boardroom and battlefield alike.', 'Sun Tzu, Thomas Cleary', 999);
