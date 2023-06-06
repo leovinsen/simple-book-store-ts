cba417c2b3f718a91e7da852196e8a5698b60972
Leonardo Vinsen <leonardovinsen@gmail.com>
2023-06-06 19:19:46 +0700

    feat: [repository] implement OrderRepository

---
+++ src/repository/orderRepository.test.ts
@@ -0,0 +1,134 @@
+import 'mocha';
+import chai from 'chai';
+import { Teardown, createTestDB } from '../database';
+import { Database } from 'sqlite3';
+import { v4 as uuidv4 } from 'uuid';
+import OrderRepository, { CreateOrder, CreateOrderDetail } from './orderRepository';
+import UserRepository from './userRepository';
+import { faker } from '@faker-js/faker';
+import { Order } from '../model/order';
+import { seedTableBooks } from '../test/seedBooks';
+import BookRepository from './bookRepository';
+import Book from '../model/book';
+
+let assert = chai.assert;
+
+describe('OrderRepository', () => {
+    let db: Database;
+    let teardown: Teardown;
+
+    let orderRepository: OrderRepository;
+    let bookRepository: BookRepository;
+    let userRepository: UserRepository;
+
+    before(async () => {
+        const database = await createTestDB(uuidv4());
+        db = database.db;
+        teardown = database.teardown;
+
+        seedTableBooks(db);
+
+        orderRepository = new OrderRepository(db);
+        bookRepository = new BookRepository(db);
+        userRepository = new UserRepository(db);
+    });
+
+    after(() => {
+        teardown();
+    });
+
+    describe('findOrders', () => {
+        it('should find all records for a given user', async () => {
+            const testUser = await userRepository.createUser(faker.internet.email(), "password");
+            if (testUser == null) {
+                throw new Error("Create test user failed");
+            }
+
+            const books = await bookRepository.getBooks();
+
+            const testOrder1 = await orderRepository.createOrder(getTestCreateOrder(testUser.id, books));
+            const testOrder2 = await orderRepository.createOrder(getTestCreateOrder(testUser.id, books));
+
+            const findOrders = await orderRepository.findOrders({ userId: testUser.id })
+            if (findOrders == null) {
+                throw Error("Expected result of findOrders to be non null");
+            }
+
+            assert.equal(findOrders!.length, 2);
+
+            const testFn = (foundOrder: Order, testOrder: Order) => {
+                assert.equal(foundOrder.id, testOrder.id);
+                assert.equal(foundOrder.grandTotal, testOrder.grandTotal);
+                assert.equal(foundOrder.userId, testUser.id);
+                assert.equal(foundOrder.userId, testOrder.userId);
+
+                for (let i = 0; i < foundOrder.details.length; i++) {
+                    const d1 = foundOrder.details[i];
+                    const d2 = testOrder.details[i];
+
+                    assert.equal(d1.bookId, d2.bookId);
+                    assert.equal(d1.sellingPrice, d2.sellingPrice);
+                    assert.equal(d1.qty, d2.qty);
+                    assert.equal(d1.subtotal, d2.subtotal);
+                }
+            }
+
+            testFn(findOrders[0], testOrder1);
+            testFn(findOrders[1], testOrder2);
+
+        });
+    });
+
+    describe('createOrder', () => {
+        it('should insert a new record into table orders successfully', async () => {
+            const testUser = await userRepository.createUser(faker.internet.email(), "password");
+            if (testUser == null) {
+                throw new Error("Create test user failed");
+            }
+
+            const books = await bookRepository.getBooks();
+
+            const testData = getTestCreateOrder(testUser.id, books);
+            const createdOrder = await orderRepository.createOrder(testData);
+
+            assert.equal(createdOrder.grandTotal, testData.grandTotal);
+            assert.equal(createdOrder.userId, testData.userId);
+
+            for (let i = 0; i < createdOrder.details.length; i++) {
+                const coDetails = createdOrder.details[i];
+                const tdDetails = testData.details[i];
+
+                assert.equal(coDetails.bookId, tdDetails.bookId);
+                assert.equal(coDetails.sellingPrice, tdDetails.sellingPrice);
+                assert.equal(coDetails.qty, tdDetails.qty);
+                assert.equal(coDetails.subtotal, tdDetails.subtotal);
+            }
+        });
+    });
+
+    // Helper function to get a randomized CreateOrder for testing purposes
+    const getTestCreateOrder = (userId: number, books: Book[]): CreateOrder => {
+        let details: CreateOrderDetail[] = [];
+        let grandTotal = 0;
+
+        for (const book of books) {
+            const qty = Math.floor(Math.random() * 5) + 1;
+            const sellingPrice = book.price;
+            const subtotal = qty * book.price;
+
+            details.push({
+                bookId: book.id,
+                sellingPrice: sellingPrice,
+                qty: qty,
+                subtotal: subtotal,
+            });
+            grandTotal += subtotal;
+        }
+
+        return {
+            userId: userId,
+            grandTotal: grandTotal,
+            details: details,
+        }
+    }
+});
---
+++ src/repository/orderRepository.ts
@@ -0,0 +1,182 @@
+import { Database } from "sqlite3";
+import { Order, OrderDetail } from "../model/order";
+import { Service } from "typedi";
+import { SortDirection, sortDirectionToSql } from "../model/sortDirection";
+
+// Defines possible filters for findOrder
+type FindOrderFilter = {
+    id?: number;
+    userId?: number;
+}
+
+type FindOrderSortOptions = {
+    createdAt?: SortDirection;
+}
+
+export class MissingFindOrderFilter extends Error {
+    constructor() {
+        super("Missing filter for findOrder method");
+    }
+}
+
+@Service()
+export default class OrderRepository {
+    private db: Database;
+
+    constructor(
+        db: Database
+    ) {
+        this.db = db;
+    }
+
+    /**
+     * Finds records in table `orders` and `orders_details` that match provided filter. They will
+     * be returned as instances of Order.
+     * 
+     * Returns an empty array if no records were found.
+     * 
+     * @param filter filters for the result set. Only one filter need to be set, and if both are set, only order.id will e used.
+     * @param sortOpts (optional) sorting options for the records. Only sortable by created_at.
+     * @returns an array containing orders that match the filter
+     */
+    public async findOrders(filter: FindOrderFilter, sortOpts?: FindOrderSortOptions): Promise<Order[]> {
+        let whereFilter: string = '';
+        let args: Array<any> = [];
+
+        if (filter.id) {
+            whereFilter = 'WHERE o.id = ?';
+            args.push(filter.id);
+        } else if (filter.userId) {
+            whereFilter = 'WHERE o.user_id = ?';
+            args.push(filter.userId);
+        }
+
+        if (sortOpts) {
+            if (sortOpts.createdAt) {
+                const orderBy: string = sortDirectionToSql(sortOpts.createdAt!);
+                whereFilter = `${whereFilter} ORDER BY ${orderBy}`;
+            }
+        }
+
+        return new Promise<Order[]>((resolve, reject) => {
+            const query = `
+            SELECT 
+                o.id,
+                o.user_id,
+                o.grand_total,
+                o.created_at,
+                od.book_id,
+                od.selling_price,
+                od.qty,
+                od.subtotal
+            FROM 
+                orders o
+            LEFT JOIN 
+                orders_details od 
+                ON od.order_id = o.id
+            ${whereFilter}
+            `
+
+            this.db.all(query, args, (err, rows: any) => {
+                if (err) {
+                    reject(err);
+                }
+
+                // no record found
+                if (rows.length == 0) {
+                    resolve([]);
+                }
+
+                const castRows = rows as [{ [key: string]: any }];
+
+                let orders: Order[] = [];
+                let orderDetails: OrderDetail[] = [];
+                let lastOrderId: number = rows[0].id;
+                let lastGrandTotal = 0;
+                let lastCreatedAt: Date = new Date();
+
+                for (let i = 0; i < castRows.length; i++) {
+                    const row = castRows[i];
+
+                    // once a new order.id is found, push the Order object into orders
+                    // and clear orderDetails array for new Order object
+                    if (row.id != lastOrderId) {
+                        orders.push(new Order(
+                            lastOrderId,
+                            rows[0].user_id,
+                            lastGrandTotal,
+                            lastCreatedAt,
+                            orderDetails,
+                        ));
+
+                        // clear array
+                        orderDetails = [];
+                        lastGrandTotal = 0;
+                        lastOrderId = row.id;
+                    }
+
+                    const detail = new OrderDetail(
+                        row.book_id,
+                        row.selling_price / 100,
+                        row.qty,
+                        row.subtotal / 100,
+                    );
+                    lastGrandTotal = row.grand_total;
+                    lastCreatedAt = new Date(row.created_at);
+
+                    orderDetails.push(detail);
+                }
+
+                // push last order to the array
+                orders.push(new Order(
+                    lastOrderId,
+                    rows[0].user_id,
+                    lastGrandTotal,
+                    lastCreatedAt,
+                    orderDetails,
+                ));
+
+                resolve(orders);
+            });
+        });
+    }
+
+    public async createOrder(order: CreateOrder): Promise<Order> {
+        const insertOrderId: number = await new Promise<number>((resolve, reject) => {
+            this.db.run("INSERT INTO orders (user_id, grand_total) VALUES (?, ?)", [order.userId, order.grandTotal], function (err: Error) {
+                if (err) {
+                    reject(err.message)
+                } else {
+                    resolve(this.lastID as number);
+                }
+            });
+        });
+
+        for (const item of order.details) {
+            const query = "INSERT INTO orders_details (order_id, book_id, selling_price, qty, subtotal) VALUES (?, ?, ?, ?, ?)";
+            const values = [insertOrderId, item.bookId, item.sellingPrice * 100, item.qty, item.subtotal * 100];
+            this.db.run(query, values);
+        }
+
+        const orders = await this.findOrders({ id: insertOrderId });
+        // should never happen, but check anyways
+        if (!orders) {
+            throw new Error("Newly created order is not found.")
+        }
+
+        return orders![0];
+    }
+}
+
+export type CreateOrder = {
+    userId: number;
+    grandTotal: number;
+    details: CreateOrderDetail[];
+}
+
+export type CreateOrderDetail = {
+    bookId: number;
+    sellingPrice: number;
+    qty: number;
+    subtotal: number;
+}
